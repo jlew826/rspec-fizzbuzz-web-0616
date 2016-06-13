ce799c22ee8a21ac7c6e44c8b7b1f7add55808aa
jlew826 <jlew826@gmail.com>
2016-06-13 15:43:27 -0400

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+def fizzbuzz(num)
+  if num % 3 == 0
+    
+    if num % 5 == 0
+      return "FizzBuzz"
+    end
+    return "Fizz"
+  elsif num % 5 == 0
+    
+    if num % 3 == 0
+      return "FizzBuzz"
+    end
+    return "Buzz"
+
+  #else
+   # return ""
+  end
+
+    
+
+end
